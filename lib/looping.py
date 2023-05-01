c0db2abefc1dee370b52f5d717d1785662c38214
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-01 13:02:20 -0400

    passing test

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while(count > 0):
+        print(count)
+        count -=1
+    print ("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return[ x **2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while (count < 101):
+        if(count%3 == 0 and count %5 == 0):
+            print("FizzBuzz")
+        elif(count%3 == 0):
+            print("Fizz")
+        elif(count%5 == 0):
+            print("Buzz")
+        else:
+            print(count)
+        count +=1
